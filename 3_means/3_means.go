16b266eeab383a695c3dce3597525c5b12840323
vincentiusvin <vincentius_vin@yahoo.com>
2025-03-11 19:48:37 +0700

    parse and test

---
+++ 3_means/3_means.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+)
+
+func main() {
+	addr := ":8000"
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Server listening at " + addr)
+
+	defer ln.Close()
+
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go means(c)
+	}
+}
+
+type Insert struct {
+	Timestamp int
+	Price     int
+}
+
+type Query struct {
+	Mintime int
+	Maxtime int
+}
+
+func parsePacket(b []byte) (*Insert, *Query) {
+	t := b[0]
+
+	firstint := b[1:5]
+	firstnum := int(binary.BigEndian.Uint32(firstint))
+
+	secint := b[5:9]
+	secnum := int(binary.BigEndian.Uint32(secint))
+
+	if t == 'Q' {
+		return nil, &Query{
+			Mintime: firstnum,
+			Maxtime: secnum,
+		}
+	} else if t == 'I' {
+		return &Insert{
+			Timestamp: firstnum,
+			Price:     secnum,
+		}, nil
+	}
+	return nil, nil
+}
+
+func means(c net.Conn) {
+	b := make([]byte, 9)
+	c.Read(b)
+}
---
+++ 3_means/3_means_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParseInsert(t *testing.T) {
+	in := []byte{0x49, 0x00, 0x00, 0x30, 0x39, 0x00, 0x00, 0x00, 0x65}
+	exp_price := 101
+	exp_timestamp := 12345
+
+	insert, _ := parsePacket(in)
+	if insert == nil {
+		t.Fatalf("Failed to parse packet")
+	}
+	if insert.Price != exp_price {
+		t.Fatalf("wrong price. got %v expected %v", insert.Price, exp_price)
+	}
+	if insert.Timestamp != exp_timestamp {
+		t.Fatalf("wrong timestamp. got %v expected %v", insert.Timestamp, exp_timestamp)
+	}
+}
+
+func TestParseQuery(t *testing.T) {
+	in := []byte{0x51, 0x00, 0x00, 0x30, 0x39, 0x00, 0x00, 0x00, 0x65}
+	exp_mintime := 12345
+	exp_maxtime := 101
+
+	_, query := parsePacket(in)
+	if query == nil {
+		t.Fatalf("Failed to parse packet")
+	}
+	if query.Mintime != exp_mintime {
+		t.Fatalf("wrong mintime. got %v expected %v", query.Mintime, exp_mintime)
+	}
+	if query.Maxtime != exp_maxtime {
+		t.Fatalf("wrong maxtime. got %v expected %v", query.Maxtime, exp_maxtime)
+	}
+}
