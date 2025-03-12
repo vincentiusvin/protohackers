448d7bb64e13c2bcd1952fc1295f8718ce436c4c
vincentiusvin <vincentius_vin@yahoo.com>
2025-03-12 19:58:49 +0700

    Should've tested earlier

---
+++ 3_chat/3_chat.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+)
+
+func main() {
+	addr := ":8000"
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Server listening at " + addr)
+
+	ch := MakeChatroom()
+
+	defer ln.Close()
+
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go ch.handleConnection(c)
+	}
+}
+
+func Remove[T comparable](s []T, elem T) []T {
+	for i, c := range s {
+		if c != elem {
+			continue
+		}
+
+		var after []T
+		if len(s) > i+1 {
+			after = s[i+1:]
+		}
+		return append(s[:i], after...)
+	}
+	return s
+}
+
+type Member struct {
+	conn net.Conn
+	name string
+}
+
+func (m *Member) Send(s string) {
+	m.conn.Write([]byte(s))
+}
+
+func (m *Member) Recv() string {
+	sc := bufio.NewScanner(m.conn)
+	for sc.Scan() {
+		return sc.Text()
+	}
+	return ""
+}
+
+type Chatroom struct {
+	members []*Member
+	mu      sync.Mutex
+}
+
+func MakeChatroom() *Chatroom {
+	return &Chatroom{
+		members: make([]*Member, 0),
+	}
+}
+
+func (ch *Chatroom) broadcast(msg string, exception ...*Member) {
+	inException := func(toCheck *Member) bool { // turn this into a hashmap if performance is bad
+		for _, member := range exception {
+			if member == toCheck {
+				return true
+			}
+		}
+		return false
+	}
+
+	for _, m := range ch.members {
+		if inException(m) {
+			continue
+		}
+		m.Send(msg)
+	}
+}
+
+func (ch *Chatroom) SendMessage(sender *Member, msg string) {
+	ch.mu.Lock()
+	defer ch.mu.Unlock()
+
+	msgFormatted := fmt.Sprintf("[%v] %v\n", sender.name, msg)
+	ch.broadcast(msgFormatted, sender)
+}
+
+func (ch *Chatroom) AddUser(m *Member) {
+	ch.mu.Lock()
+	defer ch.mu.Unlock()
+
+	m.Send(ch.UserList())
+	enterMsg := fmt.Sprintf("* %v has entered the room", m.name)
+	ch.broadcast(enterMsg)
+
+	ch.members = append(ch.members, m)
+}
+
+func (ch *Chatroom) RemoveUser(m *Member) {
+	ch.mu.Lock()
+	defer ch.mu.Unlock()
+
+	m.Send(ch.UserList())
+	enterMsg := fmt.Sprintf("* %v has left the room", m.name)
+	ch.broadcast(enterMsg, m)
+
+	var cutIdx int
+	for i, c := range ch.members {
+		if c == m {
+			cutIdx = i
+			break
+		}
+	}
+
+	ch.members = append(ch.members[:cutIdx], ch.members[cutIdx+1:]...)
+}
+
+func (ch *Chatroom) UserList() string {
+	members := make([]string, 0)
+	for _, c := range ch.members {
+		members = append(members, c.name)
+	}
+	members_string := strings.Join(members, ", ")
+
+	return "The room contains: " + members_string + "\n"
+}
+
+func (ch *Chatroom) handleConnection(c net.Conn) {
+	defer c.Close()
+
+	m := &Member{
+		conn: c,
+	}
+	m.Send("Welcome to budgetchat! What shall I call you?\n")
+	name := m.Recv()
+	if name == "" {
+		m.Send("Sorry, your name is invalid. Disconnecting now...\n")
+		return
+	}
+	m.name = name
+	ch.AddUser(m)
+}
