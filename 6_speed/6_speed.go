b0d0df1b3db27d4c6dd84eb2112b2a26be74ca69
vincentiusvin <vincentius_vin@yahoo.com>
2025-03-13 14:37:55 +0700

    parser combinators

--- 6_speed/6_speed.go
+++ 6_speed/6_speed.go
@@ -28,39 +28,30 @@ func main() {
 
 func handleConnection(c net.Conn) {
 	defer c.Close()
-	bytes := []byte{}
-	applyParser(parseUint8, bytes)
 }
 
-type ParseFunc[T any] func(b []byte) (T, int)
-
-// apply parseFunc that satisfies the definition above,
-// return a new byte slice that has been advanced
-func applyParser[T any](parseFunc ParseFunc[T], value []byte) (T, []byte) {
-	out, n := parseFunc(value)
-	return out, value[n:]
-}
+type ParseFunc[T any] func(b []byte) (T, []byte)
 
 // Consumes tokens from b to produce a string
 // Returns number of bytes consumed and the final string
-func parseString(b []byte) (string, int) {
+func parseString(b []byte) (string, []byte) {
 	len := int(b[0])
 	str := string(b[1 : len+1])
 
-	return str, len + 1
+	return str, b[len+1:]
 }
 
 // parse uint16
-func parseUint8(b []byte) (uint8, int) {
-	return uint8(b[0]), 1
+func parseUint8(b []byte) (uint8, []byte) {
+	return uint8(b[0]), b[1:]
 }
 
 // parse uint16
-func parseUint16(b []byte) (uint16, int) {
-	return binary.BigEndian.Uint16(b), 2
+func parseUint16(b []byte) (uint16, []byte) {
+	return binary.BigEndian.Uint16(b), b[2:]
 }
 
 // parse uint32
-func parseUint32(b []byte) (uint32, int) {
-	return binary.BigEndian.Uint32(b), 4
+func parseUint32(b []byte) (uint32, []byte) {
+	return binary.BigEndian.Uint32(b), b[4:]
 }
--- 6_speed/6_speed_test.go
+++ 6_speed/6_speed_test.go
@@ -4,70 +4,70 @@ import (
 	"testing"
 )
 
-func TestParseSpace(t *testing.T) {
-	type spaceCases struct {
-		in          []byte
-		expected    string
-		parsedBytes int
-	}
-
-	cases := []spaceCases{
-		{
-			in:          []byte{0x03, 0x66, 0x6f, 0x6f, 0x00, 0x05},
-			expected:    "foo",
-			parsedBytes: 4,
-		},
-		{
-			in:          []byte{0x08, 0x45, 0x6C, 0x62, 0x65, 0x72, 0x65, 0x74, 0x68, 0x01, 0x02},
-			expected:    "Elbereth",
-			parsedBytes: 9,
-		},
-	}
-	for _, c := range cases {
-		out, n := parseString(c.in)
-
-		if out != c.expected {
-			t.Fatalf("failed to parse. expected %v got %v", c.expected, out)
-		}
-		if n != c.parsedBytes {
-			t.Fatalf("consumed different bytes than expected. expected %v got %v", c.parsedBytes, n)
-		}
-	}
-
+type ParsingCases[T any] struct {
+	in       []byte
+	expected T
+	newLen   int
+	fn       ParseFunc[T]
 }
 
-func TestApplyParser(t *testing.T) {
-	type applyCases struct {
-		in       []byte
-		expected int
-		newLen   int
-	}
-
-	cases := []applyCases{
-		{
-			in:       []byte{0x03, 0x66, 0x6f, 0x6f, 0x00, 0x05},
-			expected: 0x03,
-			newLen:   5,
-		},
-		{
-			in:       []byte{0x01},
-			expected: 0x01,
-			newLen:   0,
-		},
-		{
-			in:       []byte{0x08, 0x45, 0x6C, 0x62, 0x65, 0x72, 0x65, 0x74, 0x68, 0x01, 0x02},
-			expected: 0x08,
-			newLen:   10,
-		},
-	}
+func runParsingCases[T comparable](t *testing.T, cases []ParsingCases[T]) {
 	for _, c := range cases {
-		out, new_in := applyParser(parseUint8, c.in)
-		if out != uint8(c.expected) {
+		out, new_in := c.fn(c.in)
+		if out != c.expected {
 			t.Fatalf("wrong parsing output. expected %v got %v", c.expected, out)
 		}
 		if len(new_in) != c.newLen {
 			t.Fatalf("wrong new input length. expected %v got %v", c.newLen, len(new_in))
 		}
 	}
+}
+
+func TestParser(t *testing.T) {
+	uint8Cases := []ParsingCases[uint8]{
+		{
+			in:       []byte{0x03, 0x66, 0x6f, 0x6f, 0x00, 0x05},
+			expected: 0x03,
+			newLen:   5,
+			fn:       parseUint8,
+		},
+		{
+			in:       []byte{0x01},
+			expected: 0x01,
+			newLen:   0,
+			fn:       parseUint8,
+		},
+		{
+			in:       []byte{0x08, 0x45, 0x6C, 0x62, 0x65, 0x72, 0x65, 0x74, 0x68, 0x01, 0x02},
+			expected: 0x08,
+			newLen:   10,
+			fn:       parseUint8,
+		},
+	}
+
+	stringCases := []ParsingCases[string]{
+		{
+			in:       []byte{0x03, 0x66, 0x6f, 0x6f, 0x00},
+			expected: "foo",
+			newLen:   1,
+			fn:       parseString,
+		},
+		{
+			in:       []byte{0x08, 0x45, 0x6C, 0x62, 0x65, 0x72, 0x65, 0x74, 0x68, 0x01, 0x02},
+			expected: "Elbereth",
+			newLen:   2,
+			fn:       parseString,
+		},
+	}
+
+	t.Run("uint8 cases", func(t *testing.T) {
+		runParsingCases(t, uint8Cases)
+	})
+
+	t.Run("string cases", func(t *testing.T) {
+		runParsingCases(t, stringCases)
+	})
 
 }
+
+// i love generics
