22058a3c5a973a8466e0e0df6898f0498644f499
vincentiusvin <vincentius_vin@yahoo.com>
2025-03-13 12:43:20 +0700

    solved now

--- 5_proxy/5_proxy.go
+++ 5_proxy/5_proxy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net"
 )
@@ -22,19 +23,19 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		log.Println(c.RemoteAddr(), "connected")
 		go handleConnection(c)
 	}
 }
 
-func connToChan(client net.Conn) chan string {
+func connToChan(conn net.Conn) chan string {
 	cli_to_up := make(chan string)
-	sc := bufio.NewScanner(client)
+	sc := bufio.NewScanner(conn)
 	go func() {
 		for sc.Scan() {
 			t := sc.Text()
 			cli_to_up <- t
 		}
+		fmt.Println("closed")
 		close(cli_to_up)
 	}()
 	return cli_to_up
@@ -45,13 +46,21 @@ func boguscoined(s string) string {
 }
 
 func handleConnection(client net.Conn) {
-	defer client.Close()
+	log.Println(client.RemoteAddr(), "connected")
+	defer func() {
+		client.Close()
+		log.Println(client.RemoteAddr(), "disconnected")
+	}()
 
 	upstream, err := net.Dial("tcp", "chat.protohackers.com:16963")
 	if err != nil {
 		panic(err)
 	}
-	defer upstream.Close()
+	log.Println("connected to upstream")
+	defer func() {
+		log.Println("upstream disconnected")
+		upstream.Close()
+	}()
 
 	cli_to_up := connToChan(client)
 	up_to_cli := connToChan(upstream)
@@ -60,16 +69,27 @@ func handleConnection(client net.Conn) {
 		select {
 		case cli_to_up_msg, ok := <-cli_to_up:
 			if !ok {
-				break
+				return
 			}
-			doctored_msg := boguscoined(cli_to_up_msg)
-			upstream.Write([]byte(doctored_msg))
+			fmt.Println("client say:", cli_to_up_msg)
+			doctored_msg := boguscoined(cli_to_up_msg) + "\n"
+			_, err := upstream.Write([]byte(doctored_msg))
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("relayed to upstream:", doctored_msg)
+
 		case up_to_cli_msg, ok := <-up_to_cli:
 			if !ok {
-				break
+				return
 			}
-			doctored_msg := boguscoined(up_to_cli_msg)
-			client.Write([]byte(doctored_msg))
+			fmt.Println("upstream say:", up_to_cli_msg)
+			doctored_msg := boguscoined(up_to_cli_msg) + "\n"
+			_, err := client.Write([]byte(doctored_msg))
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("relayed to client:", doctored_msg)
 		}
 	}
 }
