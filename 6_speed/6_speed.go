c6517b4505e8d528f1045131ef01be667af2eb7e
vincentiusvin <vincentius_vin@yahoo.com>
2025-03-13 15:48:47 +0700

    encode

--- 6_speed/6_speed.go
+++ 6_speed/6_speed.go
@@ -139,3 +139,36 @@ func parseUint16(b []byte) (uint16, []byte) {
 func parseUint32(b []byte) (uint32, []byte) {
 	return binary.BigEndian.Uint32(b), b[4:]
 }
+
+func encodeHeartbeat() []byte {
+	return []byte{0x41}
+}
+
+type Ticket struct {
+	Plate      string
+	Road       uint16
+	Mile1      uint16
+	Timestamp1 uint32
+	Mile2      uint16
+	Timestamp2 uint32
+	Speed      uint16
+}
+
+func encodeTicket(t *Ticket) []byte {
+	ret := append(
+		[]byte{0x21},
+		encodeString(t.Plate)...,
+	)
+	ret = binary.BigEndian.AppendUint16(ret, t.Road)
+	ret = binary.BigEndian.AppendUint16(ret, t.Mile1)
+	ret = binary.BigEndian.AppendUint32(ret, t.Timestamp1)
+	ret = binary.BigEndian.AppendUint16(ret, t.Mile2)
+	ret = binary.BigEndian.AppendUint32(ret, t.Timestamp2)
+	ret = binary.BigEndian.AppendUint16(ret, t.Speed)
+	return ret
+}
+
+func encodeString(s string) []byte {
+	l := byte(uint8(len(s)))
+	return append([]byte{l}, []byte(s)...)
+}
--- 6_speed/6_speed_test.go
+++ 6_speed/6_speed_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"slices"
 	"testing"
 )
@@ -151,4 +152,34 @@ func TestParser(t *testing.T) {
 	t.Run("hb cases", func(t *testing.T) {
 		runParsingCases(t, hbcases)
 	})
+}
+
+func TestEncode(t *testing.T) {
+	ticket := &Ticket{
+		Plate:      "UN1X",
+		Road:       66,
+		Mile1:      100,
+		Timestamp1: 123456,
+		Mile2:      110,
+		Timestamp2: 123816,
+		Speed:      10000,
+	}
+
+	out := encodeTicket(ticket)
+
+	expected := []byte{
+		0x21,
+		0x04, 0x55, 0x4e, 0x31, 0x58,
+		0x00, 0x42,
+		0x00, 0x64,
+		0x00, 0x01, 0xe2, 0x40,
+		0x00, 0x6e,
+		0x00, 0x01, 0xe3, 0xa8,
+		0x27, 0x10,
+	}
+
+	if !bytes.Equal(expected, out) {
+		t.Fatalf("failed to serialize %v", ticket)
+	}
+
 }
