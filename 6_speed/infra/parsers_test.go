ab91474e78f30867b7eaca9cb6fd0b691c95e125
vincentiusvin <vincentius_vin@yahoo.com>
2025-03-14 15:26:30 +0700

    reorg cases

--- 6_speed/infra/parsers_test.go
+++ 6_speed/infra/parsers_test.go
@@ -30,78 +30,75 @@ func runParsingCases[T any](t *testing.T, cases []ParsingCases[T]) {
 }
 
 func TestParser(t *testing.T) {
-	pleq := func(p1, p2 *infra.Plate) bool {
-		return p1.Plate == p2.Plate && p1.Timestamp == p2.Timestamp
-	}
-
-	plateCases := []ParsingCases[*infra.Plate]{
-		{
-			in: []byte{0x20, 0x04, 0x55, 0x4e, 0x31, 0x58, 0x00, 0x00, 0x03, 0xe8},
-			expected: &infra.Plate{
-				Plate:     "UN1X",
-				Timestamp: 1000,
-			},
-			newLen: 0,
-			fn:     infra.ParsePlate,
-			eq:     pleq,
-		},
-	}
-
-	cameq := func(ia1, ia2 *infra.IAmACamera) bool {
-		return ia1.Limit == ia2.Limit && ia1.Mile == ia2.Mile && ia1.Road == ia2.Road
-	}
-	cameraCases := []ParsingCases[*infra.IAmACamera]{
-		{
-			in: []byte{0x80, 0x00, 0x42, 0x00, 0x64, 0x00, 0x3c},
-			expected: &infra.IAmACamera{
-				Road:  66,
-				Mile:  100,
-				Limit: 60,
-			},
-			newLen: 0,
-			fn:     infra.ParseIAmACamera,
-			eq:     cameq,
-		},
-	}
-
-	dispeq := func(ia1, ia2 *infra.IAmADispatcher) bool {
-		return slices.Equal(ia1.Roads, ia2.Roads)
-	}
-	dispatchCases := []ParsingCases[*infra.IAmADispatcher]{
-		{
-			in: []byte{0x81, 0x03, 0x00, 0x42, 0x01, 0x70, 0x13, 0x88},
-			expected: &infra.IAmADispatcher{
-				Roads: []uint16{66, 368, 5000},
-			},
-			newLen: 0,
-			fn:     infra.ParseIAmADispatcher,
-			eq:     dispeq,
-		},
-	}
-
-	hbeq := func(wh1, wh2 *infra.WantHeartbeat) bool { return wh1.Interval == wh2.Interval }
-	hbcases := []ParsingCases[*infra.WantHeartbeat]{
-		{
-			in: []byte{0x40, 0x00, 0x00, 0x00, 0x10},
-			expected: &infra.WantHeartbeat{
-				Interval: 16,
-			},
-			newLen: 0,
-			fn:     infra.ParseWantHeartbeat,
-			eq:     hbeq,
-		},
-	}
-
 	t.Run("plate cases", func(t *testing.T) {
+		pleq := func(p1, p2 *infra.Plate) bool {
+			return p1.Plate == p2.Plate && p1.Timestamp == p2.Timestamp
+		}
+		plateCases := []ParsingCases[*infra.Plate]{
+			{
+				in: []byte{0x20, 0x04, 0x55, 0x4e, 0x31, 0x58, 0x00, 0x00, 0x03, 0xe8},
+				expected: &infra.Plate{
+					Plate:     "UN1X",
+					Timestamp: 1000,
+				},
+				newLen: 0,
+				fn:     infra.ParsePlate,
+				eq:     pleq,
+			},
+		}
 		runParsingCases(t, plateCases)
 	})
+
 	t.Run("camera cases", func(t *testing.T) {
+		cameq := func(ia1, ia2 *infra.IAmACamera) bool {
+			return ia1.Limit == ia2.Limit && ia1.Mile == ia2.Mile && ia1.Road == ia2.Road
+		}
+		cameraCases := []ParsingCases[*infra.IAmACamera]{
+			{
+				in: []byte{0x80, 0x00, 0x42, 0x00, 0x64, 0x00, 0x3c},
+				expected: &infra.IAmACamera{
+					Road:  66,
+					Mile:  100,
+					Limit: 60,
+				},
+				newLen: 0,
+				fn:     infra.ParseIAmACamera,
+				eq:     cameq,
+			},
+		}
 		runParsingCases(t, cameraCases)
 	})
 	t.Run("dispatch cases", func(t *testing.T) {
+		dispeq := func(ia1, ia2 *infra.IAmADispatcher) bool {
+			return slices.Equal(ia1.Roads, ia2.Roads)
+		}
+		dispatchCases := []ParsingCases[*infra.IAmADispatcher]{
+			{
+				in: []byte{0x81, 0x03, 0x00, 0x42, 0x01, 0x70, 0x13, 0x88},
+				expected: &infra.IAmADispatcher{
+					Roads: []uint16{66, 368, 5000},
+				},
+				newLen: 0,
+				fn:     infra.ParseIAmADispatcher,
+				eq:     dispeq,
+			},
+		}
 		runParsingCases(t, dispatchCases)
 	})
+
 	t.Run("hb cases", func(t *testing.T) {
+		hbeq := func(wh1, wh2 *infra.WantHeartbeat) bool { return wh1.Interval == wh2.Interval }
+		hbcases := []ParsingCases[*infra.WantHeartbeat]{
+			{
+				in: []byte{0x40, 0x00, 0x00, 0x00, 0x10},
+				expected: &infra.WantHeartbeat{
+					Interval: 16,
+				},
+				newLen: 0,
+				fn:     infra.ParseWantHeartbeat,
+				eq:     hbeq,
+			},
+		}
 		runParsingCases(t, hbcases)
 	})
 }
