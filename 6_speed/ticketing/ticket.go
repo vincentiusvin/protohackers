80c0abbebb8e4d9ad2c69ba91dc275e545836241
vincentiusvin <vincentius_vin@yahoo.com>
2025-03-14 19:47:07 +0700

    Main logic done, gotta do the math now

--- 6_speed/ticketing/ticket.go
+++ 6_speed/ticketing/ticket.go
@@ -1,33 +1,93 @@
 package ticketing
 
-import "protohackers/6_speed/infra"
+import (
+	"protohackers/6_speed/infra"
+	"slices"
+	"sync"
+	"time"
+)
 
-type Camera struct {
-	*infra.IAmACamera
+type Plate struct {
+	Plate     string
+	Timestamp uint32
+	Road      uint16
+	Ticketed  bool
+}
+
+type Road struct {
+	dispatchers []chan infra.Ticket
+	plates      map[string][]*Plate // map of plate number -> plate
+	limit       uint16
 }
 
 type Controller struct {
-	cameras     []*infra.IAmACamera
-	dispatchers []*infra.IAmADispatcher
-	plates      []*infra.Plate
+	roads map[uint16]*Road
+	mu    sync.Mutex
 }
 
-func MakeGlobal() *Controller {
+func MakeController() *Controller {
 	return &Controller{
-		cameras:     make([]*infra.IAmACamera, 0),
-		dispatchers: make([]*infra.IAmADispatcher, 0),
-		plates:      make([]*infra.Plate, 0),
+		roads: make(map[uint16]*Road),
 	}
 }
 
-func (g *Controller) AddCamera(cam *infra.IAmACamera) {
-	g.cameras = append(g.cameras, cam)
+func (g *Controller) getRoad(roadNum uint16) *Road {
+	if _, ok := g.roads[roadNum]; !ok {
+		g.roads[roadNum] = &Road{
+			dispatchers: make([]chan infra.Ticket, 0),
+			plates:      make(map[string][]*Plate),
+		}
+	}
+
+	return g.roads[roadNum]
 }
 
-func (g *Controller) AddDispatcher(dis *infra.IAmADispatcher) {
-	g.dispatchers = append(g.dispatchers, dis)
+func (g *Controller) UpdateLimit(roadNum uint16, limit uint16) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	rd := g.getRoad(roadNum)
+	rd.limit = limit
 }
 
-func (g *Controller) AddPlates(dis *infra.IAmADispatcher) {
+func (g *Controller) AddDispatcher(roads []uint16, ch chan infra.Ticket) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
 
+	for _, roadNum := range roads {
+		rd := g.getRoad(roadNum)
+		rd.dispatchers = append(rd.dispatchers, ch)
+	}
+}
+
+func (rd *Road) getPlateRecords(plate string) []*Plate {
+	if _, ok := rd.plates[plate]; !ok {
+		rd.plates[plate] = make([]*Plate, 0)
+	}
+	return rd.plates[plate]
+}
+
+func (g *Controller) AddPlates(plate *Plate) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	rd := g.getRoad(plate.Road)
+	recs := rd.getPlateRecords(plate.Plate)
+
+	slices.SortFunc(recs, func(a *Plate, b *Plate) int {
+		return int(a.Timestamp) - int(b.Timestamp)
+	})
+
+	for i, pl := range recs {
+		last := i - 1
+		if last < 0 && last >= len(recs) {
+			continue
+		}
+		lastPl := recs[last]
+
+		delta := lastPl.Timestamp - pl.Timestamp
+		dur := time.Duration(delta)
+		limit := rd.limit
+
+	}
 }
