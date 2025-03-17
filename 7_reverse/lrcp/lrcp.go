07c74a801e22681b6cc81fdc24ae3303c52748aa
vincentiusvin <vincentius_vin@yahoo.com>
2025-03-17 13:10:33 +0700

    add data

--- 7_reverse/lrcp/lrcp.go
+++ 7_reverse/lrcp/lrcp.go
@@ -17,7 +17,7 @@ func ParseConnect(s string) (*Connect, error) {
 	}
 	sessionNum, err := strconv.Atoi(splits[2])
 	if err != nil {
-		return nil, fmt.Errorf("cannot serialize session num to int: %w", err)
+		return nil, fmt.Errorf("cannot parse session num to int: %w", err)
 	}
 	if sessionNum < 0 {
 		return nil, fmt.Errorf("session number is negative: %v", sessionNum)
@@ -25,5 +25,41 @@ func ParseConnect(s string) (*Connect, error) {
 
 	return &Connect{
 		Session: uint(sessionNum),
+	}, nil
+}
+
+type Data struct {
+	Session uint
+	Pos     uint
+	Data    string
+}
+
+func ParseData(s string) (*Data, error) {
+	splits := strings.Split(s, "/")
+	if splits[1] != "data" {
+		return nil, fmt.Errorf("not a data request")
+	}
+	sessionNum, err := strconv.Atoi(splits[2])
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse session num to int: %w", err)
+	}
+	if sessionNum < 0 {
+		return nil, fmt.Errorf("session number is negative: %v", sessionNum)
+	}
+
+	posNum, err := strconv.Atoi(splits[3])
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse position num to int: %w", err)
+	}
+	if posNum < 0 {
+		return nil, fmt.Errorf("position number is negative: %v", sessionNum)
+	}
+
+	data := splits[4]
+
+	return &Data{
+		Session: uint(sessionNum),
+		Pos:     uint(posNum),
+		Data:    data,
 	}, nil
 }
--- 7_reverse/lrcp/lrcp_test.go
+++ 7_reverse/lrcp/lrcp_test.go
@@ -2,17 +2,36 @@ package lrcp_test
 
 import (
 	"protohackers/7_reverse/lrcp"
+	"reflect"
 	"testing"
 )
 
 func TestConnect(t *testing.T) {
 	in := "/connect/1234567"
-	var exp uint = 1234567
+	exp := &lrcp.Connect{
+		Session: 1234567,
+	}
 	c, err := lrcp.ParseConnect(in)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if c.Session != exp {
+	if !reflect.DeepEqual(c, exp) {
+		t.Fatalf("expected %v got %v", exp, c.Session)
+	}
+}
+
+func TestData(t *testing.T) {
+	in := "/data/1234567/0/hello"
+	exp := &lrcp.Data{
+		Session: 1234567,
+		Pos:     0,
+		Data:    "hello",
+	}
+	c, err := lrcp.ParseData(in)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(c, exp) {
 		t.Fatalf("expected %v got %v", exp, c.Session)
 	}
 }
