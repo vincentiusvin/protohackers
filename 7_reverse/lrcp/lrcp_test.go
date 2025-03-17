522493c6af5326a31149cc63b29d3e3c3466e762
vincentiusvin <vincentius_vin@yahoo.com>
2025-03-17 13:33:28 +0700

    add escape case

--- 7_reverse/lrcp/lrcp_test.go
+++ 7_reverse/lrcp/lrcp_test.go
@@ -25,22 +25,42 @@ func TestConnect(t *testing.T) {
 }
 
 func TestData(t *testing.T) {
-	in := "/data/1234567/0/hello/"
-	exp := &lrcp.Data{
-		Session: 1234567,
-		Pos:     0,
-		Data:    "hello",
+	type dataCases struct {
+		in  string
+		exp *lrcp.Data
 	}
-	c, err := lrcp.ParseData(in)
-	if err != nil {
-		t.Fatal(err)
+
+	cases := []dataCases{
+		{
+			in: "/data/1234567/0/hello/",
+			exp: &lrcp.Data{
+				Session: 1234567,
+				Pos:     0,
+				Data:    "hello",
+			},
+		},
+		{
+			in: "/data/1234567/0/foo\\/bar\\\\baz/",
+			exp: &lrcp.Data{
+				Session: 1234567,
+				Pos:     0,
+				Data:    "foo/bar\\baz",
+			},
+		},
 	}
-	if !reflect.DeepEqual(c, exp) {
-		t.Fatalf("expected %v got %v", exp, c.Session)
-	}
-	reencode := exp.Encode()
-	if reencode != in {
-		t.Fatalf("expected %v got %v", in, reencode)
+
+	for _, testCase := range cases {
+		c, err := lrcp.ParseData(testCase.in)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(c, testCase.exp) {
+			t.Fatalf("expected %v got %v", testCase.exp, c.Session)
+		}
+		reencode := testCase.exp.Encode()
+		if reencode != testCase.in {
+			t.Fatalf("expected %v got %v", testCase.in, reencode)
+		}
 	}
 }
 
