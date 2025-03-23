00319896b5c02b7180b0b18219038bf314bbe18b
vincentiusvin <vincentius_vin@yahoo.com>
2025-03-23 17:33:24 +0700

    get test

--- 9_queue/queue/queue_test.go
+++ 9_queue/queue/queue_test.go
@@ -92,29 +92,55 @@ func TestJobCenter(t *testing.T) {
 	ctx := context.Background()
 	jc := queue.NewJobCenter(ctx)
 
+	inPri := 123
+	inQueue := "queue1"
+	inJob := json.RawMessage([]byte("{\"f\":1}"))
+
+	var jobId int
+
 	t.Run("put test", func(t *testing.T) {
 		req := &queue.PutRequest{
 			Request: "put",
-			Queue:   "test",
-			Pri:     123,
-			Job:     []byte("{\"f\":1}"),
+			Queue:   inQueue,
+			Pri:     inPri,
+			Job:     inJob,
 		}
 		resp := jc.Put(req)
 
 		if resp.Status != queue.StatusOK {
 			t.Fatalf("failed to put")
 		}
+
+		jobId = resp.Id
 	})
 
 	t.Run("get test", func(t *testing.T) {
 		req := &queue.GetRequest{
 			Request: "get",
-			Queues:  []string{"test"},
+			Queues:  []string{inQueue},
 		}
 		resp := jc.Get(req)
 
 		if resp.Status != queue.StatusOK {
 			t.Fatalf("failed to put")
+		}
+
+		outId := *resp.Id
+		outJob := *resp.Job
+		outPri := *resp.Pri
+		outQueue := *resp.Queue
+
+		if outId != jobId {
+			t.Fatalf("id mismatch with put exp %v got %v", jobId, outId)
+		}
+		if !bytes.Equal(outJob, inJob) {
+			t.Fatalf("wrong job exp %v got %v", inJob, outJob)
+		}
+		if outPri != inPri {
+			t.Fatalf("wrong prio exp %v got %v", outPri, inPri)
+		}
+		if outQueue != inQueue {
+			t.Fatalf("wrong queue exp %v got %v", outQueue, inQueue)
 		}
 	})
 }
