cb206678afaf7744dea11eba55f95e7abfb506cb
vincentiusvin <vincentius_vin@yahoo.com>
2025-03-30 19:29:34 +0700

    i think it's done

--- 11_pest/pest/pest.go
+++ 11_pest/pest/pest.go
@@ -4,19 +4,29 @@ import (
 	"protohackers/11_pest/types"
 )
 
-type Controller struct {
+type Controller interface {
+	AddSiteVisit(sv types.SiteVisit) error
+}
+
+type CController struct {
 	sites map[uint32]Site
 }
 
-func NewController() *Controller {
-	return &Controller{
+func NewController() Controller {
+	return &CController{
 		sites: make(map[uint32]Site),
 	}
 }
 
-func (c *Controller) AddSiteVisit(sv types.SiteVisit) {
+func (c *CController) AddSiteVisit(sv types.SiteVisit) error {
 	s, err := c.getSite(sv.Site)
+	if err != nil {
+		return err
+	}
 	count, err := s.GetPops()
+	if err != nil {
+		return err
+	}
 
 	// change to map if too slow
 	for _, svEntry := range sv.Populations {
@@ -37,9 +47,11 @@ func (c *Controller) AddSiteVisit(sv types.SiteVisit) {
 			}
 		}
 	}
+
+	return nil
 }
 
-func (c *Controller) getSite(site uint32) (Site, error) {
+func (c *CController) getSite(site uint32) (Site, error) {
 	if c.sites[site] == nil {
 		ns := NewSite(site)
 		err := ns.Connect()
